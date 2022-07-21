f982358df9a3f38a3de3db1a4aee823128e0f4eb
Isaaclhy00 <97405333+Isaaclhy00@users.noreply.github.com>
2022-07-21 21:40:53 +0800

    update checklist

--- src/components/Timetable/Timetable.js
+++ src/components/Timetable/Timetable.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import "./Timetable.css";
 import { useNavigate } from "react-router-dom";
-import Iframe from 'react-iframe'
 
 const Timetable = () => {
   const navigate = useNavigate();
 
   return (
     <div>
-      <iframe src="https://nusmods.com/timetable/sem-1" height={1000} width={1850} top={20}></iframe>
+      <iframe src="https://nusmods.com/timetable/sem-1" style={{position: 'relative', height: 800, width: 1000}}></iframe>
+      <button onClick={() => navigate("/Pigeon/Dashboard")}>Back</button>
     </div>
   );
 };
